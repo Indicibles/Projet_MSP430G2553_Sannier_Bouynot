e9366bc9796bfacfd383831c8f5f657930636edd
Indicibles <80051256+Indicibles@users.noreply.github.com>
2021-03-11 14:46:31 +0100

    Update G2553_SLAVE.c

--- Projet/G2553_SLAVE.c
+++ Projet/G2553_SLAVE.c
@@ -1,6 +1,37 @@
 #include "msp430.h"
 #include <string.h>
 
+void init_pwm()
+
+{
+    WDTCTL = WDTPW + WDTHOLD;
+    P2DIR |= BIT1 | BIT2 | BIT4 | BIT5;
+    P2DIR &= ~(BIT0 | BIT3);
+    BCSCTL1 = CALBC1_1MHZ;      // choix d'horloge à 1Mhz
+    DCOCTL = CALDCO_1MHZ;
+    P2OUT |= BIT5;
+    P2OUT &= ~BIT1;
+    P2DIR |= BIT2;     //sortie des timers
+    P2SEL |= BIT2;
+    P2SEL2 &= BIT2;
+    TA1CTL = TASSEL_2 | MC_1 | ID_1 | ID_2;   // mode up
+
+    TA1CCTL1 |= OUTMOD_7;       // activation mode de sortie n°7
+
+    TA1CCR0 = 5000;             //timer
+    TA1CCR1 = 0;
+
+}
+
+void rotation(void)
+{
+    TA1CCR1 = 2500;
+}
+void stop_rotation(void)
+{
+    TA1CCR1 = 0;
+}
+
 int main(void)
 {
     WDTCTL = WDTPW + WDTHOLD;           // Stop watchdog timer
@@ -19,6 +50,7 @@ int main(void)
     UCB0CTL0  &= ~(UCMST | UCCKPH | UCCKPL | UCMSB | UC7BIT );
     UCB0CTL1 &= ~UCSWRST;                     // Initialize USCI
     IE2 |= UCB0RXIE;                          // Enable USCI0 RX interrupt
+    init_pwm();
 
     __bis_SR_register(GIE);       // Enter LPM4, enable interrupts
 
